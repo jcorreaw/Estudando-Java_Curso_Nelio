01153b1bafb22d11aa9f2c2e326114255b4af6a2
jcorreaw <juninhocorrea2014@gmail.com>
2023-06-01 12:49:55 -0300

    Leitura basica de um arquivo

---
+++ src/TrabalhandoComArquivos/Main.java
@@ -0,0 +1,27 @@
+package TrabalhandoComArquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("//home//joao//Projetos Java//testeJava.txt");
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if (sc != null){
+                sc.close();
+            }
+        }
+
+    }
+}
