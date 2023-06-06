c6494c95cff70e6d8dbf7947f487284708660cfb
jcorreaw <juninhocorrea2014@gmail.com>
2023-06-06 11:46:05 -0300

    Introdução aos Generics

---
+++ src/Generics/application/ProgramString.java
@@ -0,0 +1,26 @@
+package Generics.application;
+
+import Generics.services.PrintService;
+
+import java.util.Scanner;
+
+public class ProgramString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<String> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+    }
+}
