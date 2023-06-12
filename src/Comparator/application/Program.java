bee4e9c6c4256ac4d71722000b7f2ec32e0b2f2f
jcorreaw <juninhocorrea2014@gmail.com>
2023-06-12 17:54:21 -0300

    Comparator e lambda

---
+++ src/Comparator/application/Program.java
@@ -0,0 +1,25 @@
+package Comparator.application;
+
+import Comparator.entities.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+    }
+}
